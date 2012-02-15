266df8dfef005037c5d5a3c87536849b4983637d
junk.isidore@gmail.com <junk.isidore@gmail.com@4c58cad2-d1c6-85f9-ca8f-412d1c8b5675>
2012-02-15 18:15:49 +0000

    Fixed the Octree

---
+++ GalaxyGuide/src/ConnectionGenerator.java
@@ -0,0 +1,26 @@
+import java.util.TreeSet;
+
+
+public class ConnectionGenerator {
+	
+	public ConnectionGenerator(){
+		
+	}
+	
+	public void generateConnectionRange(TreeSet<Star> tree){
+		for(Star i:tree){
+			for(Star j:tree){
+				if(j.getName().charAt(0) < i.getName().charAt(0)-1) continue;
+				double distance = Math.sqrt((j.getPosition().x-i.getPosition().x)*(j.getPosition().x-i.getPosition().x)
+						                   +(j.getPosition().y-i.getPosition().y)*(j.getPosition().y-i.getPosition().y)
+						                   +(j.getPosition().z-i.getPosition().z)*(j.getPosition().z-i.getPosition().z));
+				if(distance > j.getRange()) {
+					double time = distance / j.getRange();
+					Connection cont = new Connection(j,time);
+					i.addConnection(cont);
+				}
+			}
+		}
+	}
+
+}
